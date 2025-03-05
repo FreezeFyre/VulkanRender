71933598c1fbd59f5bb4649947167198ae37dc59
FreezeFyre <92956453+FreezeFyre@users.noreply.github.com>
2025-03-04 17:54:38 -0800

    Initial Commit

---
+++ VulkanRender/shaders/compile.sh
@@ -0,0 +1,9 @@
+#
+//  compile.sh
+//  VulkanRender
+//
+//  Created by Luke Raykovitz on 3/8/25.
+//
+
+/Users/lukeraykovitz/VulkanSDK/1.4.304.1/macOS/bin/glslc shader.vert -o vert.spv
+/Users/lukeraykovitz/VulkanSDK/1.4.304.1/macOS/bin/glslc shader.frag -o frag.spv
---
+++ VulkanRender/shaders/shader.frag
@@ -0,0 +1,16 @@
+//
+//  shader.frag
+//  VulkanRender
+//
+//  Created by Luke Raykovitz on 3/8/25.
+//
+
+#version 450
+
+layout(location = 0) in vec3 fragColor;
+
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    outColor = vec4(fragColor, 1.0);
+}
